634ed7689afb59af093663597ea2087c47560760
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-08 20:22:03 -0300

    Retirando alguns imports nao usados

--- App.js
+++ App.js
@@ -2,19 +2,9 @@ import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
